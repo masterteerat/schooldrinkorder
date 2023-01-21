c1b0d14c368fe871fb06a7d426007383101821a0
masterteerat <teerat.saetan@gmail.com>
2023-01-21 17:22:48 +0700

    add register screen

---
+++ app/src/debug/java/com/school/project/m5/drinkorder/Registerscreen.java
@@ -0,0 +1,13 @@
+package com.school.project.m5.drinkorder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Registerscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registerscreen);
+    }
+}
